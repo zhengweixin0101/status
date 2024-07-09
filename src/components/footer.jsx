664609e9aae8505d49e17a0179f8186e158ec550
Weexy <zhengweixin0101@outlook.com>
2024-07-09 16:30:34 +0800

    删除多余import

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import { GithubOne, Home, Mail } from "@icon-park/react";
 import CustomLink from "@/components/customLink";
-import Package from "../../package.json";
 
 const Footer = () => {
   // 加载配置
-  const githubName = import.meta.env.VITE_GITHUB_NAME;
-  const homeUrl = import.meta.env.VITE_HOME_URL;
-  const emailUrl = import.meta.env.VITE_EMAIL_URL;
   const siteIcp = import.meta.env.VITE_SITE_ICP;
 
   return (
